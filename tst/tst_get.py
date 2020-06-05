9a3b74b6ed74d0b8bbd5e7555785d79f42514bca
zhxh <zhxhcoder@gmail.com>
2020-06-05 15:27:52 +0800

    restfull api tst

---
+++ tst/tst_get.py
@@ -0,0 +1,33 @@
+# coding:utf-8
+from urllib import parse
+from urllib import request
+
+url = "http://10.1.2.151/ctower-mall-c/sys/login/login.do"
+data = {
+    "id": "wdb",
+    "pwd": "wdb"
+}
+
+params = "?"
+for key in data:
+    params = params + key + "=" + data[key] + "&"
+print("Get方法参数：" + params)
+
+headers = {
+    # heard部分直接通过chrome部分request header部分
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.8',
+    'Connection': 'keep-alive',
+    'Content-Length': '14',  # get方式提交的数据长度，如果是post方式，转成get方式：【id=wdb&pwd=wdb】
+    'Content-Type': 'application/x-www-form-urlencoded'
+
+}
+
+data = parse.urlencode(data).encode('utf-8')
+# req = request.Request(url, headers=headers, data=data)  # POST方法
+req = request.Request(url+params)  # GET方法
+page = request.urlopen(req).read()
+page = page.decode('utf-8')
+
+print(page)
---
+++ tst/tst_post.py
@@ -0,0 +1,24 @@
+import json
+import urllib3
+
+post_url = "http://0.0.0.0:8383/add_task/"
+
+data = {
+    "id": "1",
+    "info": "第一",
+}
+encoded_data = json.dumps(data).encode("utf-8")
+http = urllib3.PoolManager()
+r = http.request(
+    "POST",
+    post_url,
+    body=encoded_data,
+    headers={
+        'content-type': 'application/json;charset=UTF-8'
+    }
+)
+if r.status == 200:
+    response = r.data
+    if len(response) > 0:
+        j = json.loads(response)
+        print(j)
