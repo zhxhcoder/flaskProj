b9c75d28703a8ab50a108ab0fd654de989bc4820
zhxh <zhxhcoder@gmail.com>
2020-06-16 09:56:27 +0800

    请求测试-flask网站

--- web/demo.py
+++ web/demo.py
@@ -8,20 +8,19 @@ def demo():
     if request.method == 'GET':
         return render_template('index.html', input_text='', res_text='')
     else:
-        inputText = request.form.get("input_text")
-        resText = Markup(formatRes(reverseText(inputText)))
-        return render_template('index.html', input_text=inputText, res_text=resText)
+        input_text = request.form.get("input_text")
+        res_text = Markup(format_res(reverse_text(input_text)))
+        return render_template('index.html', input_text=input_text, res_text=res_text)
 
 
-def formatRes(textList):
+def format_res(textList):
     return '<p>' + '</p><p>'.join(textList) + '</p>'
 
 
-# A sample
-def reverseText(text):
+def reverse_text(text):
     res = []
-    res.append('Original text: %s' % (text))
-    res.append('Converted text: %s' % (''.join(reversed(list(text)))))
+    res.append('原来字符串: %s' % (text))
+    res.append('转换字符串: %s' % (''.join(reversed(list(text)))))
     return res
 
 
